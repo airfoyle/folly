fc9be5b2556edf075477a4b25a4ce8955bd7710f
Drew McDermott <drew.mcdermott@yale.edu>
2014-11-02 17:07:37 -0500

    Adding? ProngPos.scala again

--- ProngPos.scala
+++ ProngPos.scala
@@ -1,5 +1,28 @@
-class Prong extends Enumeration
+import scala.collection.immutable.Vector
+
+class ProngPos(val num: Int)
 {
-  type Prong = Value
-  val A,B,C,D,E,F,G,H = Value
+  def letter: Char = ProngPos.letter(num)
+
+  override def toString: String = "ProngPos " + letter
 }
+
+object ProngPos
+{
+  val letter: Vector[Char] = Vector('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')
+}
+
+
+
+
+//// object ProngPosTest extends App
+//// {
+//// 
+//// class ProngPos extends Enumeration
+//// {
+////   type ProngPos = Value
+////   val A,B,C,D,E,F,G,H = Value
+//// }
+//// 
+////   println("Maximum ProngPos: " + ProngPos.maxId)
+//// }
